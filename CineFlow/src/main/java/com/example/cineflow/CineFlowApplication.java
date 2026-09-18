7317546079291739c3469d87dd13a72471a45a4d
Yui <tjsrma06@gmail.com>
2026-09-18 22:19:57 +0900

    feat: print greeting on startup

---
+++ CineFlow/src/main/java/com/example/cineflow/CineFlowApplication.java
@@ -0,0 +1,14 @@
+package com.example.cineflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineFlowApplication {
+
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        SpringApplication.run(CineFlowApplication.class, args);
+    }
+
+}
